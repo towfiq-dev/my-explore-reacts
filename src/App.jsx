4405c6ffd2cbc9acdacd283dcea6e9819cf84f2b
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-03-17 15:10:11 +0600

    practice custom navbar create

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import DaisiuiNav from './components/DaisiuiNav/DaisiuiNav'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+    <header>
+      <Navbar></Navbar>
+      <DaisiuiNav></DaisiuiNav>
+    </header>
+    <main>
+    
+    </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import Link from './Link';
+import { Menu, X } from 'lucide-react';
+const navigationData = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/"
+  },
+  {
+    id: 2,
+    name: "Analytics",
+    path: "/dashboard/analytics"
+  },
+  {
+    id: 3,
+    name: "Projects",
+    path: "/projects"
+  },
+  {
+    id: 4,
+    name: "Settings",
+    path: "/settings"
+  },
+  {
+    id: 5,
+    name: "Profile",
+    path: "/user/profile"
+  }
+];
+const Navbar = () => {
+  const [open, setOpen] = useState(false)
+  const links = navigationData.map(route=> <Link key={route.id}route = {route}></Link>)
+  return (
+    <div>
+    <nav className='flex justify-between mx-3 mt-3.5'>
+      <span className='flex gap-2' onClick={()=>setOpen(!open)}>
+        {open? 
+        <X className='md:hidden'></X> : 
+        <Menu className='md:hidden'></Menu>}
+        <ul className='md:hidden'>
+          {links}
+        </ul>
+        <h3>About</h3>
+      </span>
+      <ul className= 'md:flex hidden gap-4'>
+      {links}
+    </ul>
+    <button className='cursor-pointer bg-green-500 px-2 rounded'>Sign In</button>
+    </nav>
+    </div>
+  );
+};
+
+export default Navbar;
